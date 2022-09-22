b3f6e815420a48c4b4cf832e40270d8079f7e9d1
priyakapp <84661064+priyakapp@users.noreply.github.com>
2022-09-22 19:49:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ ban = os.environ.get("BAN", "")
 def echo(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
 
     if not checkuser(message):
-        app.send_message(message.chat.id, '__You are either not **Purchased Membership** or **Your Membership Expired**__',reply_to_message_id=message.id,reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("📦 Source Code", url="https://github.com/bipinkrish/Mdisk-Downloader-Bot")]]))
+        app.send_message(message.chat.id, '__You are either not **Purchased Membership** or **Your Membership Expired**__',reply_to_message_id=message.id,reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("💰 Buy Now", url="https://t.me/mdisk_owner")]]))
         return
 
     app.send_message(message.chat.id, '**Hi, I am Mdisk Video Downloader, you can watch Videos without MX Player.\n__Send me a link to Start...__**',reply_to_message_id=message.id,
